666315d242e1f1b37cd9f8848d9396e51f191cab
Tyson Cleary <tyscorp@gmail.com>
2012-02-24 02:07:51 +1100

    made player move and shit

---
+++ public/js/Player.js
@@ -0,0 +1,22 @@
+Maze.Player = (function () {
+
+	var size = 16;
+
+	var Player = function (x, y) {
+		this.x = x;
+		this.y = y;
+	};
+
+	Player.prototype.move = function (x, y) {
+		this.x += x;
+		this.y += y;
+	}
+
+	Player.prototype.draw = function (ctx) {
+		ctx.fillStyle = '#000000';
+		ctx.fillRect(this.x * size + 2, this.y * size + 2, size - 4, size - 4);
+	}
+	
+	return Player;
+	
+})();
--- public/js/maze.js
+++ public/js/maze.js
@@ -1,8 +1,47 @@
+// global context
+Maze = {};
+
 $(function () {
 
-	var c = $("c").get(0);
-	var context = c.getContext(‘2d’);
+	var canvas = $('#c').get(0);
+	var ctx = canvas.getContext('2d');
 	
+	var player = new Maze.Player(5, 5);
+	//var level = new Maze.Level();
 	
+	function gameLoop() {
+		ctx.fillStyle = '#FFFFFF';
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		
+		//level.update(player);
 	
-})();
+		player.draw(ctx);
+		//level.draw(ctx);
+		
+		//setTimeout(gameLoop, 1000/30);
+	}
+
+	$('body').keydown(function (event) {
+		switch (event.keyCode) {
+			case 37: // left
+				player.move(-1, 0);
+				gameLoop();
+				break;
+			case 38: // up
+				player.move(0, -1);
+				gameLoop();
+				break;
+			case 39: // right
+				player.move(1, 0);
+				gameLoop();
+				break;
+			case 40: // down
+				player.move(0, 1);
+				gameLoop();
+				break;
+		}
+	});
+
+	gameLoop();
+	
+}) ;
