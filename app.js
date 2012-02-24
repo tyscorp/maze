58ed1350d5e529ec2e7688ae5765617a0d1a43fb
Tyson Cleary <tyscorp@gmail.com>
2012-02-24 17:40:45 +1100

    added auto updating

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 /**
  * Module dependencies.
  */
-
-var express = require('express')
-  , routes = require('./routes')
+var express = require('express');
+var routes = require('./routes');
+var exec = require('child_process').exec;
 
 var app = module.exports = express.createServer();
 
@@ -22,6 +21,12 @@ app.configure(function(){
 // Routes
 
 app.get('/', routes.index);
+
+app.get('/update', function (req, res) {
+	exec('git stash; git pull', function (error, stdout, stderr) {
+		res.send(stdout);
+	});
+}
 
 app.listen(3003);
 
